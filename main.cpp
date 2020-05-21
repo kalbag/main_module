215fda39ddbd8b6dc41211902e3dcc85f73cf4d2
Ashwin Kalbag <ashwinkalbag@Ashwins-MacBook-Pro.local>
2020-05-21 07:40:37 -0400

    fixed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#include "foo.h"
+#include "foo.h"        // from sub1
+
+#include "bar.h"        // from sub2
 
 #include <iostream>
 
 int main()
 {
-    Foo f;
+    Foo f{ 3.14, 1, 2, "akalbag" };
+    Bar b{ 3.14, 2.65 };
     std::cout << "Hello world!" << std::endl;
     return 0;
 }
